eb7b3e5550fa5f805a627dee949ecf98e192b6f7
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-21 21:08:10 +0600

    views set up

--- BirthdayNoteTakerSwift/ViewController.swift
+++ BirthdayNoteTakerSwift/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func saveButton(_ sender: Any) {
+    }
 }
 
